7f44270ae8ca54ff66193efe829962715a060b11
loveisbug <ericnomail@gmail.com>
2016-08-15 14:57:38 +0800

    sync with private verison

--- python/crawler/lifevc.py
+++ python/crawler/lifevc.py
@@ -10,6 +10,8 @@ import xlwt
 import json
 from tqdm import tqdm
 
+category_api = 'http://app.lifevc.com/1.0/v_ios_4.0.1_28/categories/Category?itemindexid='
+iteminfo_api = 'http://app.lifevc.com/1.0/v_ios_4.0.1_28/items/itemview?iteminfoid='
 
 def fetch_item(id, output):
 	urlrequest = urllib2.Request('http://www.lifevc.com/item/' + id)
@@ -59,14 +61,18 @@ def fetch_slogan(url, output):
 			output[uid] = subdict
 
 def fetch_item_by_api(url, output):
-	pass
+	jData = json.loads(urllib2.urlopen(urllib2.Request(url)).read())
+	info = jData['InnerData']
+	output['mprice'] = info['MarketPrice']
+	output['click'] = info['ClickCount']
+	output['code'] = info['Code']
 
 def fetch_cat_by_api(url, output):
 	chlist = [2859, 2860, 2861, 2862, 2863, 2864, 2865, 2866]
-	for ch in tqdm(chlist):
+	for ch in chlist:
 		jData = json.loads(urllib2.urlopen(urllib2.Request(url + str(ch))).read())
 		cats = jData['InnerData']['Categories']
-		for cat in cats:
+		for cat in tqdm(cats):
 			for item in cat['Items']:
 				uid = item['ItemInfoId']
 				if not uid in output:
@@ -77,17 +83,28 @@ def fetch_cat_by_api(url, output):
 					subdict['slogan'] = item['Appeal']
 					subdict['price'] = item['SalePrice']
 					subdict['comment'] = item['CommentCount']
+					fetch_item_by_api(iteminfo_api + str(uid), subdict)
 					output[uid] = subdict
 
 def main():
 	vdict = {}
 	wb = xlwt.Workbook()
 	ws = wb.add_sheet('LifeVC Products')
+	ws.row(0).write(0, u'ID')
+	ws.row(0).write(1, u'商品名')
+	ws.row(0).write(2, u'slogan')
+	ws.row(0).write(3, u'售价')
+	ws.row(0).write(4, u'市场价')
+	ws.row(0).write(5, u'关注数')
+	ws.row(0).write(6, u'频道')
+	ws.row(0).write(7, u'分类-分类ID')
+	ws.row(0).write(8, u'评论数')
+	ws.row(0).write(9, u'商品代码')
 
 	# fetch_item('19735')
 	# fetch_channel('http://www.lifevc.com')
 	# fetch_slogan('http://www.lifevc.com/Exh/Topic/SelectedNProducts', vdict)
-	fetch_cat_by_api('http://app.lifevc.com/1.0/v_ios_4.0.1_28/categories/Category?itemindexid=', vdict)
+	fetch_cat_by_api(category_api, vdict)
 
 	i = 1
 	for key in vdict:
@@ -95,9 +112,12 @@ def main():
 		ws.write(i, 1, vdict[key]['name'])
 		ws.write(i, 2, vdict[key]['slogan'])
 		ws.write(i, 3, vdict[key]['price'])
-		ws.write(i, 4, vdict[key]['channel'])
-		ws.write(i, 5, vdict[key]['category'])
-		ws.write(i, 6, vdict[key]['comment'])
+		ws.write(i, 4, vdict[key]['mprice'])
+		ws.write(i, 5, vdict[key]['click'])
+		ws.write(i, 6, vdict[key]['channel'])
+		ws.write(i, 7, vdict[key]['category'])
+		ws.write(i, 8, vdict[key]['comment'])
+		ws.write(i, 9, vdict[key]['code'])
 		i += 1
 	print len(vdict)
 	wb.save('livevc.xls')
